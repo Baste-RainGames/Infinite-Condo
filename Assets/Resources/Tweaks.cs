826de9aec8a83ddcbd641bd8cef5cd6a1cf6a367
Baste Nesse Buanes <baste@rain-games.com>
2019-01-25 21:12:55 +0100

    block fall

---
+++ Assets/Resources/Tweaks.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Tweaks : ScriptableObject
+{
+    private static Tweaks _instance;
+    public static Tweaks Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Resources.Load<Tweaks>("Tweaks");
+            }
+
+            return _instance;
+        }
+    }
+
+    public float secondsBetweenMovingDown;
+}
---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private float timeOfLastMove;
+
+    private void Update()
+    {
+        if (Time.time > timeOfLastMove + Tweaks.Instance.secondsBetweenMovingDown)
+        {
+            transform.position += Vector3.down;
+            timeOfLastMove = Time.time;
+        }
+    }
+}
