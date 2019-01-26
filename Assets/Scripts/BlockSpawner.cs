af45649c421f9104db74cf18c7fe0850e6a61f8b
JagguMeg <SleepingClutch@hotmail.com>
2019-01-26 18:20:02 +0100

    preveiw done

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockSpawner : MonoBehaviour {
-    
+public class BlockSpawner : MonoBehaviour
+{
+
     public Block[] possibleBlocks;
 
     public Block[] possibleBlocksMed;
@@ -15,7 +16,7 @@ public class BlockSpawner : MonoBehaviour {
     public Transform previewspawn;
 
     private Block previewblock;
-    
+
     private Block spawnedBlock;
 
     private float difficulty;
@@ -24,95 +25,119 @@ public class BlockSpawner : MonoBehaviour {
 
     private Block preview;
     private Block previousblock;
-    private Block previousblock2; 
+    private Block previousblock2;
     private Block previousblock3;
 
-   
+
 
     private void Update()
+
+
     {
-        
-       {
-            if (difficulty <= 9999)
-            {
-                if (newpreview == true) { 
-                Block selection = possibleBlocks[Random.Range(0, possibleBlocks.Length)];
-                    if ((selection == previousblock) || (selection == previousblock2) || (selection == previousblock3)) { 
-
-                    }
-                    else
-                    {
-                        previewblock = Instantiate(selection, previewspawn.transform.position, Quaternion.identity);
-                        previewblock.enabled = false;
-
-                        previousblock3 = previousblock2;
-
-                        previousblock2 = previousblock;
-
-                        previousblock = selection;
-
-                        preview = selection;
-
-                        newpreview = false;
-                    }  
-                }
-                if (spawnedBlock == null)
-                {
-                        spawnedBlock = Instantiate(preview, spawnPoint.transform.position, Quaternion.identity);
-
-                        newpreview = true;
-                    Destroy(previewblock.gameObject);
-                }
-
-
-
-                
-            }
-            if (difficulty > 99999)
-            {
-                    Block selection = possibleBlocks[Random.Range(0, possibleBlocks.Length)];
-
-                    if ((selection == previousblock) || (selection == previousblock2) || (selection == previousblock3))
-                    {
-
-                    }
-                    else
-                    {
-                        spawnedBlock = Instantiate(selection, spawnPoint.transform.position, Quaternion.identity);
-
-                        previousblock3 = previousblock2;
-
-                        previousblock2 = previousblock;
-
-                        previousblock = selection;
-                    }
-                }
-   
-            }
-
-            if (difficulty > 9999999)
+        if (difficulty <= 9999)
+        {
+            if (newpreview == true)
             {
                 Block selection = possibleBlocks[Random.Range(0, possibleBlocks.Length)];
-
                 if ((selection == previousblock) || (selection == previousblock2) || (selection == previousblock3))
                 {
 
                 }
                 else
                 {
-                    spawnedBlock = Instantiate(selection, spawnPoint.transform.position, Quaternion.identity);
+                    previewblock = Instantiate(selection, previewspawn.transform.position, Quaternion.identity);
+                    previewblock.enabled = false;
 
                     previousblock3 = previousblock2;
 
                     previousblock2 = previousblock;
 
                     previousblock = selection;
+
+                    preview = selection;
+
+                    newpreview = false;
                 }
             }
-            
-        
+            if (spawnedBlock == null)
+            {
+                spawnedBlock = Instantiate(preview, spawnPoint.transform.position, Quaternion.identity);
 
+                newpreview = true;
+                Destroy(previewblock.gameObject);
+            }
+
+
+
+
+        }
+        if (difficulty > 99999)
+        {
+            if (newpreview == true)
+            {
+                Block selection = possibleBlocks[Random.Range(0, possibleBlocks.Length)];
+                if ((selection == previousblock) || (selection == previousblock2) || (selection == previousblock3))
+                {
+
+                }
+                else
+                {
+                    previewblock = Instantiate(selection, previewspawn.transform.position, Quaternion.identity);
+                    previewblock.enabled = false;
+
+                    previousblock3 = previousblock2;
+
+                    previousblock2 = previousblock;
+
+                    previousblock = selection;
+
+                    preview = selection;
+
+                    newpreview = false;
+                }
+            }
+            if (spawnedBlock == null)
+            {
+                spawnedBlock = Instantiate(preview, spawnPoint.transform.position, Quaternion.identity);
+
+                newpreview = true;
+                Destroy(previewblock.gameObject);
+            }
+        }
+
+        if (difficulty > 9999999)
+        {
+            if (newpreview == true)
+            {
+                Block selection = possibleBlocks[Random.Range(0, possibleBlocks.Length)];
+                if ((selection == previousblock) || (selection == previousblock2) || (selection == previousblock3))
+                {
+
+                }
+                else
+                {
+                    previewblock = Instantiate(selection, previewspawn.transform.position, Quaternion.identity);
+                    previewblock.enabled = false;
+
+                    previousblock3 = previousblock2;
+
+                    previousblock2 = previousblock;
+
+                    previousblock = selection;
+
+                    preview = selection;
+
+                    newpreview = false;
+                }
+            }
+            if (spawnedBlock == null)
+            {
+                spawnedBlock = Instantiate(preview, spawnPoint.transform.position, Quaternion.identity);
+
+                newpreview = true;
+                Destroy(previewblock.gameObject);
+            }
+        }
         difficulty = Time.time;
-
     }
 }
