8d6d958423f7fac99767d6bf50d190b8d38f00ce
JagguMeg <SleepingClutch@hotmail.com>
2019-01-27 00:57:55 +0100

    mainmenu

---
+++ Assets/Scripts/ScreenRes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenRes : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public void setfullscreen()
+    {
+        Screen.SetResolution(width, height, true);
+    }
+
+    public void setwindowed()
+    {
+        Screen.SetResolution(width, height, false);
+    }
+  
+
+}
