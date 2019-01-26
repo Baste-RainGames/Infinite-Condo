6ccae4b0704669c4290870b0788349ebee0a0d97
Baste Nesse Buanes <baste@rain-games.com>
2019-01-26 15:28:23 +0100

    More prefabs!

--- Assets/Scripts/BlockDataPiece.cs
+++ Assets/Scripts/BlockDataPiece.cs
@@ -5,10 +5,13 @@ public struct BlockDataPiece {
     public int xOffset;
     public int yOffset;
 
-    public bool stairR0, stairR90, stairR180, stairR270;
+    public bool stairR0, stairR90;
 
-    public bool stairL0   => stairR270;
-    public bool stairL90  => stairR0;
-    public bool stairL180 => stairR90;
-    public bool stairL270 => stairR180;
+    public bool stairR180 => stairR0;
+    public bool stairR270 => stairR90;
+
+    public bool stairL0   => stairR90;
+    public bool stairL90  => stairR180;
+    public bool stairL180 => stairR270;
+    public bool stairL270 => stairR0;
 }
